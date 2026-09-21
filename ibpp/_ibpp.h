aa0abe0b69d7df0e7d27ef3f64ff118c83c7eddf
Marinus-Syntri <m.vandenberg@syntri.nl>
2026-09-21 08:04:24 +0200

    Define _WIN32 and __WIN32__ for IBPP_WINDOWS

--- ibpp/_ibpp.h
+++ ibpp/_ibpp.h
@@ -56,6 +56,12 @@
 #endif
 
 #ifdef IBPP_WINDOWS
+#ifndef _WIN32
+#define _WIN32
+#endif
+#ifndef __WIN32__
+#define __WIN32__
+#endif
 #include <windows.h>
 #endif
 
@@ -1066,11 +1072,11 @@ private:
 	DatabaseImpl* mDatabase;		// Attached database
 	TransactionImpl* mTransaction;	// Attached transaction
 	RowImpl* mInRow;
-	//bool* mInMissing;			// Quels paramètres n'ont pas été spécifiés
+	//bool* mInMissing;			// Quels paramÃ¨tres n'ont pas Ã©tÃ© spÃ©cifiÃ©s
 	RowImpl* mOutRow;
 	bool mResultSetAvailable;	// Executed and result set is available
 	bool mCursorOpened;			// dsql_set_cursor_name was called
-	IBPP::STT mType;			// Type de requète
+	IBPP::STT mType;			// Type de requÃ¨te
 	std::string mSql;			// Last SQL statement prepared or executed
 
 	// Internal Methods
@@ -1255,8 +1261,8 @@ private:
 	ISC_QUAD			mId;
 	bool				mDescribed;
 	ISC_ARRAY_DESC		mDesc;
-	DatabaseImpl*  		mDatabase;		// Database attachée
-	TransactionImpl*	mTransaction;	// Transaction attachée
+	DatabaseImpl*  		mDatabase;		// Database attachÃ©e
+	TransactionImpl*	mTransaction;	// Transaction attachÃ©e
 	void*				mBuffer;		// Buffer for native data
 	int					mBufferSize;	// Size of this buffer in bytes
 	int					mElemCount;		// Count of elements in this array
